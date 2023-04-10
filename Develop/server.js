03ea3cdd4739e95580d688aaad74830deaec30aa
JayStepz <jakob_s_stephens@hotmail.com>
2023-04-10 16:08:52 -0400

    added some notes and made small tweaks. not sure if it will work

--- Develop/server.js
+++ Develop/server.js
@@ -13,16 +13,23 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-app.get('/api/index', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+// GET route for start page
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
+// GET route for notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
+// POST route for creating notes
 app.post('/notes', createNote);
+
+// POST route for rendering notes
 app.post('/notes', getNotes);
+
+// POST route for deleting notes
 app.delete('/notes:id', deleteNote);
 
 app.listen(PORT, () => 
